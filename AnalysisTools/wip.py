1edc3f466fcbf7a87e40f19217749aa71c6cd696
Avi Aizenman <aviaizenman@Avis-MacBook-Pro.local>
2017-02-10 11:26:49 -0800

    remove old

--- AnalysisTools/wip.py
+++
@@ -1,6 +0,0 @@
-### Eyetrack Calibration between Blocks
-
-# In between blocks provide option, 'Press [1] to calibrate, [2] to continue'
-
-# If pressed [1], calibrate. Open up calibration stimuli:
-
